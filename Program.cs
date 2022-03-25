9eec67025922eb55b17dfeaa0fe78630b076881b
Jill Oakes <jericho_rules@hotmail.com>
2022-03-25 14:14:55 -0400

    Complete Method Overloading Exercise

--- Program.cs
+++ Program.cs
@@ -4,9 +4,51 @@ namespace MethodOverloading
 {
     internal class Program
     {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var addition = num1 + num2;
+            if (isTrue == true && addition == 1)
+            {
+                return $"{addition} dollar";
+            }
+            else if (isTrue == true && (addition > 1 || addition < 1))
+            {
+                return $"{addition} dollars";
+            }
+            else
+            {
+                //return "It's not clear what to return when false.";
+                return addition.ToString();
+            }
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a method named Add, that accepts two integers and returns the sum of those two integers.
+            Console.WriteLine("This is the method overloading exercise.");
+            Console.WriteLine($"Result from Add method with integers: {Add(6, 4)}");
+
+            // Now create an overload of the Add method to account for being able to add two decimals together.
+
+            Console.WriteLine($"Result from Add method with decimals: {Add(2.22m, 4.46m)}");
+
+            // Now create another overload of the Add method that returns a string and accepts 3 parameters: 2 integers and 1 boolean.
+            // If the boolean parameter is equal to true, the Add method will return the sum of the 2 numbers plus the word “dollars” at the end of the string.
+            // You must also account for the appropriate grammar of the string - so it can’t be “1 dollars”
+            // For example: “5 dollars”, "1 dollar”, “16 dollars”.
+
+            Console.WriteLine($"Result from Add Method with 3 parameters: {Add(3, 2,true)}");
+            Console.WriteLine($"Result from Add Method with 3 parameters: {Add(0, 1, true)}");
+            Console.WriteLine($"Result from Add Method with 3 parameters: {Add(0, 0, true)}");
+            Console.WriteLine($"Result from Add Method with 3 parameters but false: {Add(7, 9, false)}");
         }
     }
 }
